8acbbc3cbee8f650ac460ca605f8b213c88aae17
Bjorn Persson Mattsson <bjorn.pm@plankter.se>
2014-02-06 16:32:44 +0100

    Added the three main modules with empty manager files

---
+++ Source/ProductionModule/ProductionManager.cpp
@@ -0,0 +1,63 @@
+#include "ProductionManager.h"
+
+
+ProductionManager::ProductionManager(void)
+{
+}
+
+
+ProductionManager::~ProductionManager(void)
+{
+}
+
+
+void ProductionManager::onStart() {
+}
+
+void ProductionManager::onEnd(bool isWinner) {
+}
+
+void ProductionManager::onFrame() {
+}
+
+void ProductionManager::onSendText(std::string text) {
+}
+
+void ProductionManager::onReceiveText(BWAPI::Player player, std::string text) {
+}
+
+void ProductionManager::onPlayerLeft(BWAPI::Player player) {
+}
+
+void ProductionManager::onNukeDetect(BWAPI::Position target) {
+}
+
+void ProductionManager::onUnitDiscover(BWAPI::Unit unit) {
+}
+
+void ProductionManager::onUnitEvade(BWAPI::Unit unit) {
+}
+
+void ProductionManager::onUnitShow(BWAPI::Unit unit) {
+}
+
+void ProductionManager::onUnitHide(BWAPI::Unit unit) {
+}
+
+void ProductionManager::onUnitCreate(BWAPI::Unit unit) {
+}
+
+void ProductionManager::onUnitDestroy(BWAPI::Unit unit) {
+}
+
+void ProductionManager::onUnitMorph(BWAPI::Unit unit) {
+}
+
+void ProductionManager::onUnitRenegade(BWAPI::Unit unit) {
+}
+
+void ProductionManager::onSaveGame(std::string gameName) {
+}
+
+void ProductionManager::onUnitComplete(BWAPI::Unit unit) {
+}
---
+++ Source/StrategyModule/StrategyManager.cpp
@@ -0,0 +1,63 @@
+#include "StrategyManager.h"
+
+
+StrategyManager::StrategyManager(void)
+{
+}
+
+
+StrategyManager::~StrategyManager(void)
+{
+}
+
+
+void StrategyManager::onStart() {
+}
+
+void StrategyManager::onEnd(bool isWinner) {
+}
+
+void StrategyManager::onFrame() {
+}
+
+void StrategyManager::onSendText(std::string text) {
+}
+
+void StrategyManager::onReceiveText(BWAPI::Player player, std::string text) {
+}
+
+void StrategyManager::onPlayerLeft(BWAPI::Player player) {
+}
+
+void StrategyManager::onNukeDetect(BWAPI::Position target) {
+}
+
+void StrategyManager::onUnitDiscover(BWAPI::Unit unit) {
+}
+
+void StrategyManager::onUnitEvade(BWAPI::Unit unit) {
+}
+
+void StrategyManager::onUnitShow(BWAPI::Unit unit) {
+}
+
+void StrategyManager::onUnitHide(BWAPI::Unit unit) {
+}
+
+void StrategyManager::onUnitCreate(BWAPI::Unit unit) {
+}
+
+void StrategyManager::onUnitDestroy(BWAPI::Unit unit) {
+}
+
+void StrategyManager::onUnitMorph(BWAPI::Unit unit) {
+}
+
+void StrategyManager::onUnitRenegade(BWAPI::Unit unit) {
+}
+
+void StrategyManager::onSaveGame(std::string gameName) {
+}
+
+void StrategyManager::onUnitComplete(BWAPI::Unit unit) {
+}

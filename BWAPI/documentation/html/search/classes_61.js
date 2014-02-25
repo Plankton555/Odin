87c5a0d802fae06403b62f2f4821ccc7e7f65f01
Björn Mattsson <bjorn.pm@plankter.se>
2014-02-25 08:08:54 +0100

    Removed project in order to merge with other branch

--- BWAPI/documentation/html/search/classes_61.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['aimodule',['AIModule',['../class_b_w_a_p_i_1_1_a_i_module.html',1,'BWAPI']]]
-];
--- BWAPI/documentation/html/search/classes_6f.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['order',['Order',['../class_b_w_a_p_i_1_1_order.html',1,'BWAPI']]]
-];

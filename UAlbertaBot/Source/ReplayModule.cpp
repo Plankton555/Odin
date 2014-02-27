e4a54cc8a4ae1677063a426f6b28588c653d90fa
Florian Minges <florianminges@gmail.com>
2014-02-27 17:17:46 +0100

    Automated replays <Florian | Filip>
    Important notice: there is a bug in bwapi, due to which we always have to run these replays with an odd number of replays in the folder. For more information on the issue, see issue 497.
    http://code.google.com/p/bwapi/issues/detail?id=497
    
    (tried to solve it, but failed at compiling the old bwapi-version. It is a small work-around, which I find to be decent enough)

--- UAlbertaBot/Source/ReplayModule.cpp
+++ UAlbertaBot/Source/ReplayModule.cpp
@@ -7,7 +7,9 @@ ReplayModule::~ReplayModule() {}
 
 void ReplayModule::onStart()
 {
-	Broodwar->printf("Hello, my name is Odin! And this is a replay");
+	Broodwar->printf("Hello, my name is Odin! This is a replay.");
+	Broodwar->printf("Map: %s \n", BWAPI::Broodwar->mapFileName().c_str());
+	Broodwar->printf("Map: %s \n", BWAPI::Broodwar->mapPathName().c_str());
 }
 
 void ReplayModule::onFrame()

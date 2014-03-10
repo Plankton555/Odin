8b1a6dc11c79d5b07e5aa2a1ec40795eee1105f8
Bjorn Persson Mattsson <bjorn.pm@plankter.se>
2014-03-10 16:49:04 +0100

    Added comments code skeleton in BNetParser

--- UAlbertaBot/Source/BNetParser.cpp
+++ UAlbertaBot/Source/BNetParser.cpp
@@ -33,6 +33,7 @@ void BNetParser::start_element(const unsigned long line_number, const std::strin
 	else if (strcmp(cname, "state") == 0)
 	{
 		// Each state will be enumerated here (eg. Absent/Present or Periodx)
+		// state name will be in atts.reset(); atts.move_next(); atts.element().value();
 	}
 	else if (strcmp(cname, "parents") == 0)
 	{
@@ -40,6 +41,7 @@ void BNetParser::start_element(const unsigned long line_number, const std::strin
 	}
 	else if (strcmp(cname, "probabilities") == 0)
 	{
+		// Prepare to receive probabilities in "characters" function
 	}
 	ofstream myfile("bnet_debug.txt", ios::app);
 	//BWAPI::Broodwar->printf("on line %i we hit the <%s> tag", line_number, name);
@@ -59,6 +61,20 @@ void BNetParser::start_element(const unsigned long line_number, const std::strin
 
 void BNetParser::end_element(const unsigned long line_number, const std::string& name)
 {
+	const char *cname = name.c_str();
+	if (strcmp(cname, "probabilities") == 0)
+	{
+		// Received the probabilities for the current node
+	}
+	else if (strcmp(cname, "parents") == 0)
+	{
+		// Received the parents for the current node
+	}
+	else if (strcmp(cname, "cpt") == 0)
+	{
+		// finished with the node
+	}
+
 	ofstream myfile("bnet_debug.txt", ios::app);
 	//cout << "on line " << line_number << " we hit the closing tag </" << name << ">" << endl;
 	myfile << "on line " << line_number << " we hit the closing tag </" << name << ">" << endl;
@@ -67,6 +83,9 @@ void BNetParser::end_element(const unsigned long line_number, const std::string&
 
 void BNetParser::characters(const std::string& data)
 {
+	// If we're waiting for parents, extract parents from data (separated by a ' ')
+	// If we're waiting for probabilities, extract from data
+
 	ofstream myfile("bnet_debug.txt", ios::app);
 	//BWAPI::Broodwar->printf("Got some data between tags and it is: s");
 	//cout << "Got some data between tags and it is:\n" << data << endl;
@@ -76,6 +95,7 @@ void BNetParser::characters(const std::string& data)
 
 void BNetParser::processing_instruction(const unsigned long line_number, const std::string& target, const std::string& data)
 {
+	// Can ignore
 	ofstream myfile("bnet_debug.txt", ios::app);
 	//cout << "on line " << line_number << " we hit a processing instruction with a target of '" << target << "' and data '" << data << "'" << endl;
 	myfile << "on line " << line_number << " we hit a processing instruction with a target of '" << target << "' and data '" << data << "'" << endl;

1b2b3cbee83517000672f1e70d67d70670cc0d70
AftonTroll <filbry_92@hotmail.com>
2014-04-12 19:28:34 +0200

    Added a basic load/save of cases

--- UAlbertaBot/Source/DataModule.cpp
+++ UAlbertaBot/Source/DataModule.cpp
@@ -8,68 +8,97 @@
 #define COUNTER_NAMES_POSITION (2)
 #define NR_FUZZY_VALUES  (FUZZY_VALUES_END - FUZZY_VALUES_START)
 const std::string FUZZY_VALUES_FILEPATH = ODIN_DATA_FILEPATH + "fuzzy_units.txt";
+const std::string CASES_FILEPATH = ODIN_DATA_FILEPATH + "cases.txt";
 const std::string SPLIT_SYMBOL = ",";
 const std::string SUB_SPLIT_SYMBOL = ".";
 const char COMMENT_CHAR = ';';
 
 using namespace std;
 
-std::map<const char*,std::vector<int>*> *DataModule::units = NULL;
-std::map<const char*,std::vector<BWAPI::UnitType>* > *DataModule::counters = NULL;
+std::map<const char*,std::vector<int>*>* DataModule::units = NULL;
+std::map<const char*,std::vector<BWAPI::UnitType>* >* DataModule::counters = NULL;
+std::map<std::string,std::string>* DataModule::cases = NULL;
 int DataModule::loaded = 0;
 
 void DataModule::init()
 {
 	if (loaded == 0)
 	{
-		std::string line;
-		int nrFiles = 0;
-		std::ifstream unitfile (FUZZY_VALUES_FILEPATH.c_str());
-		if (unitfile.is_open())
-		{
-			units = new std::map<const char*, std::vector<int>*>;
-			counters = new std::map<const char*, std::vector<BWAPI::UnitType>* >;
+		loadFuzzy();
+		loadCase();
+		loaded = 1;
+	}
+}
 
-			while (getline(unitfile,line)) {
-				if (*line.c_str() != COMMENT_CHAR) //Ignore comments
+void DataModule::loadFuzzy()
+{
+	std::string line;
+	std::ifstream unitfile (FUZZY_VALUES_FILEPATH.c_str());
+	if (unitfile.is_open())
+	{
+		units = new std::map<const char*, std::vector<int>*>;
+		counters = new std::map<const char*, std::vector<BWAPI::UnitType>* >;
+
+		while (getline(unitfile,line)) {
+			if (*line.c_str() != COMMENT_CHAR) //Ignore comments
+			{
+				//split the line
+				std::vector<std::string>* sub = splitDelim(line, SPLIT_SYMBOL);
+
+				//Save fuzzy values
+				std::vector<std::string>* readValues = splitDelim(sub->at(FUZZY_VALUES_POSITION), SUB_SPLIT_SYMBOL);
+				std::vector<int>* fuzzyValues = new std::vector<int>(readValues->size());
+				for (int i = 0; i < readValues->size(); i++)
 				{
-					//split the line
-					std::vector<std::string>* sub = splitDelim(line, SPLIT_SYMBOL);
+					fuzzyValues->at(i) = atoi(readValues->at(i).c_str());
+				}
+				(*units)[sub->at(0).c_str()] = fuzzyValues;
 
-					//Save fuzzy values
-					std::vector<std::string>* readValues = splitDelim(sub->at(FUZZY_VALUES_POSITION), SUB_SPLIT_SYMBOL);
-					std::vector<int>* fuzzyValues = new std::vector<int>(readValues->size());
-					for (int i = 0; i < readValues->size(); i++)
+				//Save the counters
+				std::vector<std::string>* readCounters = splitDelim(sub->at(COUNTER_NAMES_POSITION), SUB_SPLIT_SYMBOL);
+				if (readCounters->size() == 1 && strcmp(readCounters->at(0).c_str(),"None") == 0) //There are no counters
+				{
+					(*counters)[sub->at(0).c_str()] = NULL;
+				} else
+				{
+					std::vector<BWAPI::UnitType>* counterNames = new std::vector<BWAPI::UnitType>(readCounters->size());
+
+					for (int i = 0; i < readCounters->size(); i++)
 					{
-						fuzzyValues->at(i) = atoi(readValues->at(i).c_str());
+						counterNames->at(i) = BWAPI::UnitTypes::getUnitType(readCounters->at(i).c_str());
 					}
-					(*units)[sub->at(0).c_str()] = fuzzyValues;
-
-					//Save the counters
-					std::vector<std::string>* readCounters = splitDelim(sub->at(COUNTER_NAMES_POSITION), SUB_SPLIT_SYMBOL);
-					if (readCounters->size() == 1 && strcmp(readCounters->at(0).c_str(),"None") == 0) //There are no counters
-					{
-						(*counters)[sub->at(0).c_str()] = NULL;
-					} else
-					{
-						std::vector<BWAPI::UnitType>* counterNames = new std::vector<BWAPI::UnitType>(readCounters->size());
-
-						for (int i = 0; i < readCounters->size(); i++)
-						{
-							counterNames->at(i) = BWAPI::UnitTypes::getUnitType(readCounters->at(i).c_str());
-						}
-						(*counters)[sub->at(0).c_str()] = counterNames;
-					}
+					(*counters)[sub->at(0).c_str()] = counterNames;
 				}
 			}
-
-
-			unitfile.close();
-			loaded = 1;
-		} else
-		{
-			BWAPI::Broodwar->printf("Unable to open file.");
 		}
+		unitfile.close();
+		loaded = 1;
+	} else
+	{
+		BWAPI::Broodwar->printf("Unable to open fuzzy file.");
+	}
+}
+
+void DataModule::loadCase()
+{
+	std::string line;
+	std::ifstream file (CASES_FILEPATH.c_str());
+	if (file.is_open())
+	{
+		cases = new std::map<std::string, std::string>;
+
+		while (getline(file,line)) {
+			if (*line.c_str() != COMMENT_CHAR) //Ignore comments
+			{
+				//split the line
+				std::vector<std::string>* sub = splitDelim(line, SPLIT_SYMBOL);
+				(*cases)[sub->at(0)] = sub->at(1);
+			}
+		}
+		file.close();
+	} else
+	{
+		BWAPI::Broodwar->printf("Unable to open cases file.");
 	}
 }
 
@@ -86,6 +115,30 @@ void DataModule::destroy()
 		delete units; //delete the map itself
 	}
 	loaded = 0;
+}
+
+std::map<std::string, std::string>* DataModule::getCases()
+{
+	return cases;
+}
+
+bool DataModule::saveCases()
+{
+	if (loaded)
+	{
+		std::ofstream file (CASES_FILEPATH.c_str());
+		if (file.is_open())
+		{
+			std::map<std::string, std::string>::iterator it;
+			for (it = cases->begin(); it != cases->end(); it++)
+			{
+				file << it->first << "," << it->second << endl;
+			}
+			file.close();
+			return true;
+		}
+	}
+	return false;
 }
 
 std::map<const char*,std::vector<int>*>* DataModule::getFuzzyValues()
--- UAlbertaBot/Source/DataModule.h
+++ UAlbertaBot/Source/DataModule.h
@@ -9,13 +9,17 @@ public:
 	static void init();
 	static void destroy();
 	static std::map<const char*,std::vector<int>*>* getFuzzyValues();
-	static std::vector<BWAPI::UnitType> *						getCounter(std::string unit);
+	static std::map<std::string, std::string>* getCases();
+	static std::vector<BWAPI::UnitType>* getCounter(std::string unit);
+	static bool saveCases();
 
 	static int loaded;
 private:
 	static std::vector<std::string>* splitDelim(const std::string& str, const std::string& delim);
+	static void loadFuzzy();
+	static void loadCase();
 
-	static std::map<const char*,std::vector<int>*> *units;
-	static std::map<const char*,std::vector<BWAPI::UnitType>* > *counters;
-	
+	static std::map<std::string,std::string>* cases;
+	static std::map<const char*,std::vector<int>*>* units;
+	static std::map<const char*,std::vector<BWAPI::UnitType>* >* counters;
 };

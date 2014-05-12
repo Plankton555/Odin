d9cd63c611199b4974afa073c822c295fa4c056b
Bjorn Persson Mattsson <bjorn.pm@plankter.se>
2014-05-12 16:28:09 +0200

    Testing for uncertaintyFactor = 1.5

--- Odin/Source/StrategyManager.cpp
+++ Odin/Source/StrategyManager.cpp
@@ -71,7 +71,7 @@ StrategyManager & StrategyManager::Instance()
 
  void StrategyManager::updateState()
  {
-	double enemyUncertaintyFactor = 1.25;
+	double enemyUncertaintyFactor = 1.5;
 	double myEconomy = getEconomyPotential(BWAPI::Broodwar->self());
 	double myArmy = getArmyPotential(BWAPI::Broodwar->self(), myEconomy);
 	double myDefense = getDefensePotential(BWAPI::Broodwar->self());
